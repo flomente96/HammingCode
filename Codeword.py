612e5e5196bba0311de4cddd6fdcff8adddd3e6b
faielomente <frlomente@up.edu.ph>
2016-03-25 01:51:47 +0800

    for testing

--- Codeword.py
+++ Codeword.py
@@ -2,9 +2,9 @@
 
 class Codeword:
 
-    def checkbits_needed(self, databits):
+    def checkbits_needed(self, data_bits):
         """From the equation m + r + 1 <= 2^r"""
-        m = len(databits)
+        m = len(data_bits)
         r = 1
 
         while m + r + 1 > 2**r:
@@ -13,28 +13,26 @@ class Codeword:
 
     def create_parity(self, databits, r):
         code = []
-        codelength = len(databits) + r
-        datalength = len(databits)
+        code_length = len(databits) + r
+        data_length = len(databits)
         exponent = 0
         j = 0
         i = 1
 
-        while i <= codelength:
+        while i <= code_length:
             if i == 2**exponent:
                 code.append('_')
                 exponent += 1
-            elif j < datalength:
+            elif j < data_length:
                 code.append(databits[j])
                 j += 1
             i += 1
 
-        print(code)
-
         return code
 
-    def create_codeword(self, array, even):
+    def create_codeword(self, array, iseven):
         i = 0
-        if even is True:
+        if iseven is True:
             while i < len(array):
                 if array[i] == '_':
                     temp = self.extract_bits(array, i + 1)
@@ -44,7 +42,7 @@ class Codeword:
                         array[i] = '1'
                 i += 1
 
-        elif even is False:
+        elif iseven is False:
             while i < len(array):
                 if array[i] == '_':
                     temp = self.extract_bits(array, i + 1)
@@ -69,35 +67,31 @@ class Codeword:
             ctr += 1
             i += 1
 
-        # print("Extracted")
-        # print(use_bits)
         return use_bits
 
     def count(self, extracted):
         return extracted.count('1')
 
-    def error_detection(self, codeword,  even):
+    def error_detection(self, codeword,  iseven):
         error = []
         i = 0
         exponent = 0
 
-        if even is True:
+        if iseven is True:
             while i < len(codeword):
                 if i == (2**exponent)-1:
                     temp = self.extract_bits(codeword, i + 1)
-                    # print(codeword[i])
-                    # print(temp)
                     if self.count(temp) % 2 != 0:
                         error.append(i+1)
                     exponent += 1
                 i += 1
 
-        elif even is False:
+        elif iseven is False:
             while i < len(codeword):
                 if i == (2**exponent)-1:
                     temp = self.extract_bits(codeword, i + 1)
                     if self.count(temp) % 2 == 0:
-                        error.append(i)
+                        error.append(i+1)
                     exponent += 1
                 i += 1
         print(error)
@@ -105,47 +99,57 @@ class Codeword:
 
 
 """===============================   MAIN   ======================================"""
-# todo: check the error_detector function
-# PART I:
-# length = int(input("Data bits Length: "))
-# index = 0
-transmitted = "10011010"
-ed = Codeword()
-#
-# while index < length:
-#     transmitted += input()
-#     index += 1
-#
-r = ed.checkbits_needed(transmitted)
-# print("r = % i" % r)
-temp = ed.create_parity(transmitted, r)
-transmitted = "".join(ed.create_codeword(temp, True))
-print("Codeword (transmitted): ", transmitted)
-# print("PARITY\n1. Even Parity\n2. Odd Parity")
-
-# PART II:
+#------------------------ PART I -----------------------------
 index = 0
-exp = 0
-length = len(transmitted)
-print("Length:", length)
-temp = list(transmitted)
+transmitted = ""
+ed = Codeword()
+print("***********************PART I***********************")
+length = int(input("Data bits Length: "))
+print("Input DATA BITS:")
 while index < length:
-    if index != (2**exp)-1:
-        temp[index] = input()
-    else:
-        print(temp[index])
-        exp += 1
+    transmitted += input()
     index += 1
 
-received = "".join(temp)
-print("Codeword (receiver): ", received)
-temp = ed.error_detection(received, True)
-if len(temp) == 0:
+checkbits = ed.checkbits_needed(transmitted)
+temp = ed.create_parity(transmitted, checkbits)
+print("DATA bits + CHECK bits:\n", temp)
+choice = int(input("Choose PARITY\t1. Even Parity\t2. Odd Parity\n"))
+if choice == 1:
+    even = True
+elif choice == 2:
+    even = False
+else:
+    raise ValueError("Out of range. Only 1 or 2 are the responses allowed.")
+transmitted = "".join(ed.create_codeword(temp, even))
+print("Codeword(transmitted): ", transmitted)
+
+#------------------------ PART II ----------------------------
+index = 0
+exp = 0
+received = ""
+print("\n***********************PART II**********************")
+length = int(input("Data bits Length: "))
+print("Input CODEWORD:")
+while index < length:
+    received += input()
+    index += 1
+
+print("Codeword(receiver): ", received)
+choice = int(input("Choose PARITY\t1. Even Parity\t2. Odd Parity\n"))
+if choice == 1:
+    even = True
+elif choice == 2:
+    even = False
+else:
+    raise ValueError("Out of range. Only 1 or 2 are the responses allowed.")
+
+t = ed.error_detection(received, even)
+if len(t) == 0:
     print("No error!")
 else:
     index = 0
     bit = 0
-    while index < len(temp):
-        bit += temp[index]
+    while index < len(t):
+        bit += t[index]
         index += 1
     print("Erroneous bit: ", bit)
